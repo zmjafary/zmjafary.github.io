2b58078fcbf4b549a59a4c110f6a6251ae69c040
Zille Muhammad Jafary <zmjafary@yahoo.com>
2024-09-09 22:38:52 +0500

    tag manager added v2

--- gatsby-config.js
+++ gatsby-config.js
@@ -144,7 +144,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: 'UA-45666519-2',
+        trackingId: 'G-T5TKLHCFBD',
       },
     },
   ],
